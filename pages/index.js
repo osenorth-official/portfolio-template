629c7266bb93117d5dfc7667429590fbf50d91ee
Parag Katoch <paragkatoch5@gmail.com>
2022-01-23 23:10:00 +0530

    added userData

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,16 @@
+import ActivityCard from "@/components/ActivityCard";
+import FrontFace from "@/components/FrontFace";
+import Header from "@/components/Header";
+import WorkSection from "@/components/WorkSection";
+import { front_face, header, workExperience } from "@/utils/userData";
 import styles from "../styles/Home.module.scss";
 
 export default function Home() {
-	return <div className={styles.app}></div>;
+	return (
+		<div className={styles.app}>
+			<Header {...header} />
+			<FrontFace {...front_face} />
+			<WorkSection workExperience={workExperience} />
+		</div>
+	);
 }
---
+++ utils/userData.js
@@ -0,0 +1,93 @@
+export const header = {
+	logoName: "HOME",
+
+	navItems: [
+		{
+			name: "WORK",
+			link: "/",
+		},
+		{
+			name: "CONTACT",
+			link: "/",
+		},
+	],
+};
+
+export const front_face = {
+	about:
+		"Hey! I'm Henry, a twenty-four years old User Interface and Experience Designer currently based in California.",
+
+	intro: [
+		{
+			name: "Recent Accomplishments",
+			type: "text",
+			contents: [
+				"Learned HTML + CSS",
+				"Started a side project",
+				"Cooked meal for myself and choked to death",
+			],
+		},
+		{
+			name: "Social Links",
+			type: "link",
+			contents: [
+				{ name: "Instagram", link: "/" },
+				{ name: "Dribbble", link: "/" },
+				{ name: "Twitter", link: "/" },
+				{ name: "Github", link: "/" },
+				{ name: "Youtube", link: "/" },
+				{ name: "Spotify", link: "/" },
+			],
+		},
+		{
+			name: "Currently",
+			type: "text",
+			contents: ["Doing contract/freelance work", "Available for new projects"],
+		},
+	],
+};
+
+export const workExperience = [
+	{
+		infoData: {
+			Company: ["Apple"],
+			Description: [
+				"Looking to improve the overall quality of the product through a design thinking approach, as well as conducting Usability sessions to ensure the product appeals to our users.",
+			],
+			"My Objectives": ["User Testing", "Accessibility improvements"],
+		},
+
+		image: {
+			src: "/images/apple.png",
+			alt: "osenorth",
+		},
+	},
+	{
+		infoData: {
+			Company: ["Apple"],
+			Description: [
+				"Looking to improve the overall quality of the product through a design thinking approach, as well as conducting Usability sessions to ensure the product appeals to our users.",
+			],
+			"My Objectives": ["User Testing", "Accessibility improvements"],
+		},
+
+		image: {
+			src: "/images/spiderman.png",
+			alt: "osenorth",
+		},
+	},
+	{
+		infoData: {
+			Company: ["Apple"],
+			Description: [
+				"Looking to improve the overall quality of the product through a design thinking approach, as well as conducting Usability sessions to ensure the product appeals to our users.",
+			],
+			"My Objectives": ["User Testing", "Accessibility improvements"],
+		},
+
+		image: {
+			src: "/images/microsoft.png",
+			alt: "osenorth",
+		},
+	},
+];
