cbf7bb49ee30c3f2f16b8d34072152682d82de12
bivin-88 <80203330+bivin-88@users.noreply.github.com>
2022-01-27 21:02:00 +0400

    Update data.js

--- data.js
+++ data.js
@@ -1,11 +1,12 @@
 /* eslint-disable import/no-anonymous-default-export */
 export default {
+	name: "Bivin",
 	email: "bivinjoseph121@gmail.com",
 	about:
 		"Hey! I'm <b>Bivin Joseph</b>, a 19 years old <b>UX/UI Designer</b> and <b>Product Designer</b> currently based in <b>India</b>.",
 
 	// intro
-	Skills: [
+	accomplishments: [
 		"Problem Solving",
 		"User Research",
 		"User Interface Design",
@@ -19,8 +20,8 @@ export default {
 		{ name: "Youtube", link: "/https://www.youtube.com/watch?v=6Nb-prB-4P0" },
 		{ name: "Spotify", link: "/" },
 	],
-	Experience: [
-		"Incuwise Startup Incubator(UX/UI Designer) || May 2021-Present"
+	currentProfession: [
+		"Incuwise Startup Incubator(UX/UI Designer) || May 2021-Present",
 		"Freelance UX/UI Designer || Sept 2020-Present",
 		"Member of Pixelate (Design Club) || Jan 2021-Present",
 	],
@@ -33,36 +34,39 @@ export default {
 		},
 		{ name: "Discord", value: "Bivin#3483" },
 	],
-	
+	currentStatus: [
+		"Walking my dog in the park",
+		"Making dog excuse to gawk at the hot girl",
+	],
 
 	// work
 	// image min-dimensions: 1640x600
 	// image format: jpg, png, webp
 	// if image is from an external source then add the domain in next.config.js
-	Projects: [
+	workExperience: [
 		{
-			Project Name: "Metride",
+			company: "Metride",
 			description:
 				"METRIDE, an online alternative to metro cards that helps the users to book metro in an easier and convenient way giving a great user experience.",
-			Role: ["UX/UI Designer"],
+			objectives: ["UX/UI Designer"],
 			link: "https://www.notion.so/Solution-to-Design-challenge-1-Metro-Mania-cdb974f2f90c4dd48701b2879d2f3ede",
-			image: "public/images/metride.png",
+			image: "/images/metride.png",
 		},
 		{
-			Project Name: "Newo App || Cab Booking App",
+			company: "Newo App || Cab Booking App",
 			description:
 				"NEWO is the only platform across the globe to integrate instant messaging and cab hailing on a single application. Users get the immense connectivity and convenience alongside a list of astonishing features, all specially designed for stylish iPhones & user-friendly Android devices.",
-			Role: ["UX/UI Designer"],
+			objectives: ["UX/UI Designer"],
 			link: "https://www.notion.so/Newo-App-Cab-Booking-App-dee781afcb5247728a9cf46d2512a065",
-			image: "public/images/newo.png",
+			image: "/images/newo.png",
 		},
 		{
-			Project Name: "Panda App || Dating App ",
+			company: "Panda App || Dating App ",
 			description:
-				"PANDA will be a ‘dating app’ without being called a dating app. It will incorporate a test and certain tangible metrics that may allow/disallow people onto the PANDA framework, ensuring that most—if not all—users are an appropriate match for another.",
-			Role: ["UX/UI Designer"],
+				"PANDA will be a 'dating app' without being called a dating app. It will incorporate a test and certain tangible metrics that may allow/disallow people onto the PANDA framework, ensuring that most—if not all—users are an appropriate match for another.",
+			objectives: ["UX/UI Designer"],
 			link: "https://www.notion.so/Panda-App-Dating-App-51a1ba9ffbb94dc69b1bae86e342b455",
-			image: "public/images/panda.png",
+			image: "/images/panda.png",
 		},
 	],
 };
